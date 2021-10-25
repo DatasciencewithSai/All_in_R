1dfbe1338ff8a7053b608c9742bd962ceb2e574f
sainathod <sainathdaakuri@Sainaths-MacBook-Pro.local>
2021-10-25 19:33:49 -0400

    first Commit

---
+++ Connect_to_Git.R
@@ -0,0 +1,17 @@
+## Connect to Git from R studio
+
+## step 1
+
+##go to terminal and make sure you have git installed on PC
+##to check it use git --version
+
+##step 2
+
+##create git hub account
+## connect to github from terminal using
+##git config --global user.name "sainathod"
+##git config --global user.mail "sainath2214@gmail.com
+
+## step 3
+
+## with in Rstudio Create new project
