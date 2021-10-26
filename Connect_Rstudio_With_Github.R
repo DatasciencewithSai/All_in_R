b7adf50e46cff1ecc45832dc2f3f554b877ab1a3
sainathod <sainathdaakuri@Sainaths-MacBook-Pro.local>
2021-10-25 20:23:57 -0400

    Second Commit

---
+++ Connect_Rstudio_With_Github.R
@@ -0,0 +1,22 @@
+
+## steps to conect R studio with Github 
+
+## create github account and a repo in git 
+
+## create a pat in git for password 
+
+## step 1
+
+##go to terminal and make sure you have git installed on PC
+##to check it use git --version
+
+##step 2
+
+##create git hub account
+## connect to github from terminal using
+##git config --global user.name "sainathod"
+##git config --global user.mail "sainath2214@gmail.com
+
+## step 3
+
+## with in Rstudio Create new project
